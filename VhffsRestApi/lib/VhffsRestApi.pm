1a3c736b9885cca475d4b06b88edf6ffde71ef9a
Laurent Bives <laurent@sloter.org>
2012-10-15 19:59:02 +0200

    Implement get and create verbs for user resource.

--- VhffsRestApi/lib/VhffsRestApi.pm
+++ VhffsRestApi/lib/VhffsRestApi.pm
@@ -1,5 +1,22 @@
+#
+#   Copyright © 2012 APINC Devel Team
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 package VhffsRestApi;
 use Dancer ':syntax';
+use VhffsRestApi::HTTP::Status;
 
 our $VERSION = '0.1';
 
@@ -13,14 +30,45 @@ use Vhffs::Object;
  
 my $vhffs = new Vhffs;
  
-get '/' => sub {
-    template 'index';
+get '/api/user/:username' => sub {
+    my $user = Vhffs::User::get_by_username( $vhffs , params->{username} );
+    return status_not_found ( 'User ' . params->{username} . ' not found' ) unless defined $user;
+    status_ok ( { username => params->{username} , email => $user->{mail} } );
 };
 
-get '/user/:username' => sub {
-    my $user = Vhffs::User::get_by_username( $vhffs , params->{username} );
-    return 'user unknown' unless(defined $user);
-    return { username => params->{username} , email => $user->{mail} };
+post '/api/user/' => sub {
+    my $params = request->params;
+    my ($username, $passwd, $mail) = ($params->{username}, $params->{passwd}, $params->{mail});
+
+    if ( !defined $username or !defined $passwd or !defined $mail ) {
+        return status_bad_request ( 'Missing argument (needs username, passwd and mail)' );
+    }
+
+    if ( not Vhffs::User::check_username($username) ) {
+        return status_bad_request ( 'Not a valid vhffs username' );
+    }
+
+    my $user = Vhffs::User::get_by_username( $vhffs , $username );
+
+    if ( defined $user ) {
+       return status_conflict ( 'User already exists' );
+    }
+
+    $user = Vhffs::User::create($vhffs, $username, $passwd, 0, $mail);
+
+    if ( !defined $user )
+    {
+        return status_conflict ( 'Unable to create user ' . $username );
+    }
+    else
+    {
+        return status_ok ( 'User '. $username . ' created' );
+    }
+};
+
+# default route shall remain last one :)
+any qr{.*} => sub {
+    status_not_found ( 'Not found' );
 };
 
 true;
