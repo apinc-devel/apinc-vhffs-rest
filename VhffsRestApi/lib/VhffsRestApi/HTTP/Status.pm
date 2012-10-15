5dd290e3e9c51947a7b710bd46e6a197952495b5
Laurent Bives <laurent@sloter.org>
2012-10-15 19:57:56 +0200

    Add HTTP Status codes module.

---
+++ VhffsRestApi/lib/VhffsRestApi/HTTP/Status.pm
@@ -0,0 +1,121 @@
+#
+#   Copyright © 2012 APINC Devel Team
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Inspired Dancer::Plugin::REST
+# http://cpansearch.perl.org/src/SUKRIA/Dancer-Plugin-REST-0.07/lib/Dancer/Plugin/REST.pm
+
+package VhffsRestApi::HTTP::Status;
+use strict;
+use warnings;
+
+use Dancer ':syntax';
+use Dancer::Plugin;
+
+our $VERSION   = '0.01';
+
+register send_entity => sub {
+    my ($entity, $http_code) = @_;
+
+    $http_code ||= 200;
+
+    status($http_code);
+    $entity;
+};
+
+my %http_codes = (
+
+    # 1xx
+    100 => 'Continue',
+    101 => 'Switching Protocols',
+    102 => 'Processing',
+
+    # 2xx
+    200 => 'OK',
+    201 => 'Created',
+    202 => 'Accepted',
+    203 => 'Non-Authoritative Information',
+    204 => 'No Content',
+    205 => 'Reset Content',
+    206 => 'Partial Content',
+    207 => 'Multi-Status',
+    210 => 'Content Different',
+
+    # 3xx
+    300 => 'Multiple Choices',
+    301 => 'Moved Permanently',
+    302 => 'Found',
+    303 => 'See Other',
+    304 => 'Not Modified',
+    305 => 'Use Proxy',
+    307 => 'Temporary Redirect',
+    310 => 'Too many Redirect',
+
+    # 4xx
+    400 => 'Bad Request',
+    401 => 'Unauthorized',
+    402 => 'Payment Required',
+    403 => 'Forbidden',
+    404 => 'Not Found',
+    405 => 'Method Not Allowed',
+    406 => 'Not Acceptable',
+    407 => 'Proxy Authentication Required',
+    408 => 'Request Time-out',
+    409 => 'Conflict',
+    410 => 'Gone',
+    411 => 'Length Required',
+    412 => 'Precondition Failed',
+    413 => 'Request Entity Too Large',
+    414 => 'Request-URI Too Long',
+    415 => 'Unsupported Media Type',
+    416 => 'Requested range unsatisfiable',
+    417 => 'Expectation failed',
+    418 => 'Teapot',
+    422 => 'Unprocessable entity',
+    423 => 'Locked',
+    424 => 'Method failure',
+    425 => 'Unordered Collection',
+    426 => 'Upgrade Required',
+    449 => 'Retry With',
+    450 => 'Parental Controls',
+
+    # 5xx
+    500 => 'Internal Server Error',
+    501 => 'Not Implemented',
+    502 => 'Bad Gateway',
+    503 => 'Service Unavailable',
+    504 => 'Gateway Time-out',
+    505 => 'HTTP Version not supported',
+    507 => 'Insufficient storage',
+    509 => 'Bandwidth Limit Exceeded',
+);
+
+for my $code (keys %http_codes) {
+    my $helper_name = lc($http_codes{$code});
+    $helper_name =~ s/[^\w]+/_/gms;
+    $helper_name = "status_${helper_name}";
+
+    register $helper_name => sub {
+        if ($code >= 400) {
+            send_entity({error => $_[0]}, $code);
+        }
+        else {
+            send_entity($_[0], $code);
+        }
+    };
+}
+
+register_plugin;
+1;
