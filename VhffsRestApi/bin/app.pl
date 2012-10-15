a20e08b17d1f172f2de7b2501ea9a39efc5f75c1
Laurent Bives <laurent@sloter.org>
2012-10-15 13:39:46 +0200

    Initial GET user resource.

--- VhffsRestApi/bin/app.pl
+++ VhffsRestApi/bin/app.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/env perl
 use Dancer;
+
+# changing default settings
+# By default, Dancer uses config.yml for the application's settings.
+# But we can easily change them from our script, and not use config.yml at all:
+set warnings => false;
+set serializer => 'JSON';
+
+use strict;
 use VhffsRestApi;
+
 dance;
--- VhffsRestApi/lib/VhffsRestApi.pm
+++ VhffsRestApi/lib/VhffsRestApi.pm
@@ -3,8 +3,24 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
+use strict;
+use lib '/usr/share/vhffs/api';
+
+use Vhffs::User;
+use Vhffs::Group;
+use Vhffs;
+use Vhffs::Object;
+ 
+my $vhffs = new Vhffs;
+ 
 get '/' => sub {
     template 'index';
+};
+
+get '/user/:username' => sub {
+    my $user = Vhffs::User::get_by_username( $vhffs , params->{username} );
+    return 'user unknown' unless(defined $user);
+    return { username => params->{username} , email => $user->{mail} };
 };
 
 true;
